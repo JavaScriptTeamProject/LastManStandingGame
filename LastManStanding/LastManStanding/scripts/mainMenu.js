c9bc7c5549418b1008991ff01ab98c56c119cabf
NikolaBozhkov <n.bozhkov.ads@gmail.com>
2014-06-18 20:56:03 +0300

    Menu completed

--- LastManStanding/LastManStanding/scripts/mainMenu.js
+++ LastManStanding/LastManStanding/scripts/mainMenu.js
@@ -8,6 +8,19 @@ var showMenu = function () {
     }
 }
 
+var switchDisplay = function (goingBack) {
+    goingBack = goingBack || false;
+
+    if (goingBack) {
+        $('#game-menu').css('z-index', 1).show();
+        $('#game-info').css('z-index', -1).hide();
+    }
+    else {
+        $('#game-menu').css('z-index', -1).hide();
+        $('#game-info').css('z-index', 1).show();
+    }
+};
+
 var $buttons = $('.menu-button');
 
 var $playButton = $($buttons[0]);
@@ -15,6 +28,7 @@ var $restartButton = $($buttons[1]);
 var $controlsButton = $($buttons[2]);
 var $loreButton = $($buttons[3]);
 var $creditsButton = $($buttons[4]);
+var $backButton = $($buttons[5]);
 
 var $controls = $($('#game-info').children()[0]);
 var $lore = $($('#game-info').children()[1]);
@@ -48,7 +62,24 @@ $restartButton.on('click', function () {
 });
 
 $controlsButton.on('click', function () {
-    $('#game-menu').css('z-index', -1).hide();
-    $('#game-info').css('z-index', 1).show();
+    switchDisplay();
     $controls.show();
+    $controls.addClass('show');
+});
+
+$loreButton.on('click', function () {
+    switchDisplay();
+    $lore.show();
+    $lore.addClass('show');
+});
+
+$creditsButton.on('click', function () {
+    switchDisplay();
+    $credits.show();
+    $credits.addClass('show');
+});
+
+$backButton.on('click', function () {
+    switchDisplay(true);
+    $('.show').hide().removeClass('show');
 });
